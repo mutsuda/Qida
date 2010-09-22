79d3e5d9b3e34a465a47ed29868fb192f54092c2
Masumi <mutsuda@gmail.com>
2010-09-22 02:00:03 +0200

    Primer Qida commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qida::Application.config.secret_token = 'dfee02dbee554c70bd008431f3c6f2fd1419d4391e635d91bc640160df594dbe957ef71de24dac4064e9985fd89a43d9f06b8d02e37bce24cb7d4469d1b482ba'
